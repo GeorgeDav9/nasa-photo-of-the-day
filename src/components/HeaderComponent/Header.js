be7c51acc42db342ae6802fd682cce9c88327586
George Davila <georgedavila@Georges-MacBook-Pro.local>
2019-12-12 18:49:13 -0500

    finished mvp

--- src/components/HeaderComponent/Header.js
+++ src/components/HeaderComponent/Header.js
@@ -2,16 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const WhiteH1 = styled.h1`
-    color: white;
+    color: gray;
 `;
 const Header = () => {
     return (
-        <div></div>
-        // <section className = 'header'>
-        //     <div className = 'heading'>
-        //     <WhiteH1>NASA Photo Of The Day</WhiteH1>
-        //     </div>
-        // </section>
+        <div>
+         <section className = 'header'>
+             <div className = 'heading'>
+             <WhiteH1>NASA Photo Of The Day</WhiteH1>
+             </div>
+         </section>
+        </div>
     );
 }
 
